e551832fb7ba00d62bb6fb9af7d1d19b27d40a91
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-05-23 17:07:23 -0300

    Serializando e desserializando a classe Item

--- egg plant brownie/Models/Item.swift
+++ egg plant brownie/Models/Item.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class Item: NSObject {
+class Item: NSObject, NSCoding {
     
     let nome: String
     let calorias: Double
     
+    // MARK: - init
     init(nome: String, calorias: Double) {
         self.nome = nome
         self.calorias = calorias
     }
+    
+    // MARK: - NSCondimg
+    func encode(with coder: NSCoder) {
+        coder.encode(nome, forKey: "nome")
+        coder.encode(calorias, forKey: "calorias")
+    }
+    
+    required init?(coder: NSCoder) {
+        nome = coder.decodeObject(forKey: "nome") as! String
+        calorias = coder.decodeDouble(forKey: "calorias")
+    }
+    
 }
